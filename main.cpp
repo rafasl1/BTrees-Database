11a342d407cb410dc79e600be67f2aea3d8d8618
Rafael <rafael.sl@usp.br>
2020-04-25 23:55:11 -0300

    Base project running

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 int main(){
+    
+    char entrada[100]; // a maior entrada do Freire tinha 79
+    const char fechar[] = "FECHAR";
+    
+    do {
+
+        cin >> entrada;
+        cout << "Si fode ai cara" << endl;
+
+    } while(strcmp(entrada,fechar));
+
+    cout << "Vc saiu do programa";
+
     return 0;
 }
